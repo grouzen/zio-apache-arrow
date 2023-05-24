810ce610c20f970b784e23aea3d671709031b22a
Michael Nedokushev <grouzen.hexy@gmail.com>
2023-05-24 19:21:43 +0100

    Remove vector package entirely

--- modules/core/src/main/scala/me/mnedokushev/zio/apache/arrow/core/vector/VectorError.scala
+++
@@ -1,6 +0,0 @@
-package me.mnedokushev.zio.apache.arrow.core.vector
-
-final case class VectorError(
-  message: String,
-  cause: Option[Throwable] = None
-) extends IllegalStateException(message, cause.getOrElse(new IllegalStateException()))
