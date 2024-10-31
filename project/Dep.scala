928e28014f61dab9b9e9c9dc23a4d60690aec843
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-10-31 15:23:19 +0100

    Update arrow-format, arrow-memory, ... to 18.0.0 (#122)

--- project/Dep.scala
+++ project/Dep.scala
@@ -5,7 +5,7 @@ object Dep {
   object V {
     val zio                   = "2.1.11"
     val zioSchema             = "1.5.0"
-    val arrow                 = "17.0.0"
+    val arrow                 = "18.0.0"
     val scalaCollectionCompat = "2.12.0"
   }
 
