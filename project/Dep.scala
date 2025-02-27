40c17282137b9327fade9338cbfefcdd8e2e79cf
Scala Steward <scala_steward@virtuslab.com>
2025-02-27 15:07:02 +0000

    Update zio, zio-test, zio-test-sbt to 2.1.16

--- project/Dep.scala
+++ project/Dep.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dep {
 
   object V {
-    val zio                   = "2.1.15"
+    val zio                   = "2.1.16"
     val zioSchema             = "1.6.3"
     val arrow                 = "18.2.0"
     val scalaCollectionCompat = "2.13.0"
