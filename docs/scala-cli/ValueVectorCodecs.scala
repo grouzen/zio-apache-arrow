68b881333cf262f156dd060ec9eca431eb6fd5f0
Scala Steward <scala_steward@virtuslab.com>
2025-05-14 16:31:42 +0000

    Update arrow-format, arrow-memory, ... to 18.3.0

--- docs/scala-cli/ValueVectorCodecs.scala
+++ docs/scala-cli/ValueVectorCodecs.scala
@@ -1,6 +1,6 @@
 //> using scala "3.4.3"
 //> using dep me.mnedokushev::zio-apache-arrow-core:0.1.8
-//> using dep org.apache.arrow:arrow-memory-unsafe:18.2.0
+//> using dep org.apache.arrow:arrow-memory-unsafe:18.3.0
 //> using javaOpt --add-opens=java.base/java.nio=ALL-UNNAMED
 
 import me.mnedokushev.zio.apache.arrow.core.Allocator
