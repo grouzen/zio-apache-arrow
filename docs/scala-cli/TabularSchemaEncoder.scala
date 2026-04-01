0b3f951d40eda54024abc3682ba11f7575b7d2f8
Scala Steward <scala_steward@virtuslab.com>
2026-04-01 15:41:20 +0000

    Update scala3-library to 3.8.3

--- docs/scala-cli/TabularSchemaEncoder.scala
+++ docs/scala-cli/TabularSchemaEncoder.scala
@@ -1,4 +1,4 @@
-//> using scala "3.4.3"
+//> using scala "3.8.3"
 //> using dep me.mnedokushev::zio-apache-arrow-core:0.1.8
 //> using dep org.apache.arrow:arrow-memory-unsafe:18.3.0
 //> using javaOpt --add-opens=java.base/java.nio=ALL-UNNAMED
