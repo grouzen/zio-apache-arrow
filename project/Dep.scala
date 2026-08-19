b7e5775ba118943567eae989d6c4b7c0db1f0167
Scala Steward <scala_steward@virtuslab.com>
2026-08-19 16:48:22 +0000

    Update arrow-format, arrow-memory, ... to 19.0.0

--- project/Dep.scala
+++ project/Dep.scala
@@ -5,7 +5,7 @@ object Dep {
   object V {
     val zio                   = "2.1.19"
     val zioSchema             = "1.7.2"
-    val arrow                 = "18.3.0"
+    val arrow                 = "19.0.0"
     val scalaCollectionCompat = "2.14.0"
   }
 
